649793c9babf4881a06826d10454ac6efb75cbb0
orioldedios <orioldedios@gmail.com>
2018-10-15 16:43:06 +0200

    la vida que pos eso, la vida

--- AI Class 04/class6/Assets/MoveToMouseClick.cs
+++ AI Class 04/class6/Assets/MoveToMouseClick.cs
@@ -1,21 +1,33 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.AI;
 
 public class MoveToMouseClick : MonoBehaviour {
 
 	public LayerMask mask;
 	public int mouse_button = 0;
 
-	// Update is called once per frame
-	void Update () {
+    public GameObject[] objects;
+
+    // Update is called once per frame
+    void Update () {
 		if(Input.GetMouseButton(mouse_button))
 		{
 			RaycastHit hit;
 			Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
 			if(Physics.Raycast(r, out hit, 10000.0f, mask) == true)
 				transform.position = hit.point;
-		}
+
+            for (int i = 0; i < objects.Length; i++)
+            {
+                if (objects[i].GetComponent<NavMeshAgent>() != null)
+                {
+                    objects[i].GetComponent<NavMeshAgent>().destination = transform.position;
+                }
+
+            }
+        }
 	}
 
 }
---
+++ AI Class 04/class6/Assets/targetMesh.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class targetMesh : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
