938e351bc67cf361dc88c29d934c9dacd750475a
orioldedios <orioldedios@gmail.com>
2018-10-19 12:00:55 +0200

    fsdfasdfa

---
+++ Animation/Assets/Stop.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class Stop : MonoBehaviour {
+
+	public NavMeshAgent navMeshAgent;
+	public Animator animation;
+    public float distance;
+
+	// Use this for initialization
+	void Start () 
+	{
+		navMeshAgent = GetComponent<NavMeshAgent>();
+		animation = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        distance = navMeshAgent.remainingDistance;
+
+
+        if (navMeshAgent.remainingDistance > 1.0f) {
+			animation.SetBool("movement",true);	 	
+		}
+        else
+        {
+            animation.SetBool("movement", false);
+        }
+
+        Vector3 vel = 
+    }
+}
