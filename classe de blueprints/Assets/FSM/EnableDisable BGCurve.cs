d8bbf8e6003f084ef2143f0387f4be9f893a4519
orioldedios <orioldedios@gmail.com>
2018-11-05 17:07:35 +0100

    dfsdf

---
+++ classe de blueprints/Assets/FSM/EnableDisable BGCurve.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+
+[Name("Enable BG")]
+[Category("Custom")]
+
+public class EnableDisableBGCurve : ActionTask {
+
+    public GameObject bgCurve;
+
+    protected override void OnExecute()
+    {
+        bgCurve.SetActive(true);
+    }
+
+    protected override void OnStop()
+    {
+        bgCurve.SetActive(false);
+    }
+}
